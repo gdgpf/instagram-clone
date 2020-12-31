dcf25c9ce04a7bf166628c2e40baef56f08e6c01
Vinícius Boscardin <boscardinvinicius@gmail.com>
2020-12-31 10:25:21 -0300

    .

--- packages/api/controller/profile/index_test.go
+++ packages/api/controller/profile/index_test.go
@@ -3,37 +3,43 @@ package profile
 import (
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/steinfletcher/apitest"
 )
 
-var getUser = apitest.NewMock().
-	Get("/user/12345").
-	RespondWith().
-	Body(`{"name": "jon", "id": "1234"}`).
-	Status(http.StatusOK).
-	End()
-
-var getPreferences = apitest.NewMock().
-	Get("/preferences/12345").
-	RespondWith().
-	Body(`{"is_contactable": true}`).
-	Status(http.StatusOK).
-	End()
-
-func TestApi(t *testing.T) {
+func init() {
 	err := godotenv.Load("../../.env.development")
 	if err != nil {
 		log.Fatal("Não foi possível localizar o arquivo .env")
 	}
+}
 
+func TestApiLib(t *testing.T) {
 	apitest.New().
-		Handler(http.HandlerFunc(Create)).
-		Post("/aaaa").
-		Body(`{"username": "vnnb", "description": "aaaaaaaa"}`).
+		// Report(apitest.SequenceDiagram()).
+		Handler(http.HandlerFunc(Index)).
+		Get("/profile").
 		Expect(t).
 		Status(http.StatusOK).
 		End()
+}
+
+func TestWhitoutLib(t *testing.T) {
+	r := mux.NewRouter()
+	r.HandleFunc("/profile", Index)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+	t.Run("Deve retornar 200", func(t *testing.T) {
+		res, err := http.Get(ts.URL + "/profile")
+		if err != nil {
+			t.Errorf("Expected nil, received %s", err.Error())
+		}
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("Expected %d, received %d", http.StatusNotFound, res.StatusCode)
+		}
+	})
 }
